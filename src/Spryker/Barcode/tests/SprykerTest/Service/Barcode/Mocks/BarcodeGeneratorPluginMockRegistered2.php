0ffb1972eaa3c88d31b4e1f243fbad560bbe0dbf
Ruslan Dovhospynyi <dovhospynyi@MacBook-Pro-15-Dovgospiniy.local>
2018-07-12 15:56:41 +0300

    Revert "Revert "Merge branch 'master' into feature/ps-1995-event-resource-plugins""
    This reverts commit b9e49b1cd70b491637a8a69eb69c79b90a4a26d3.

--- src/Spryker/Barcode/tests/SprykerTest/Service/Barcode/Mocks/BarcodeGeneratorPluginMockRegistered2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SprykerTest\Service\Barcode\Mocks;
-
-class BarcodeGeneratorPluginMockRegistered2 extends AbstractBarcodeGeneratorPluginMock
-{
-    public const GENERATED_CODE = 'mock_registered_2_generated_code';
-    public const GENERATED_ENCODING = 'mock_registered_2_generated_encoding';
-}
--- src/Spryker/Offer/tests/SprykerTest/Zed/Offer/_support/Helper/OfferDataHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SprykerTest\Zed\Offer\Helper;
-
-use Codeception\Module;
-
-class OfferDataHelper extends Module
-{
-}
